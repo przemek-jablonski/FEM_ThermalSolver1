a222a5f2aa342e4adf632289c0d4f5d124baf297
Przemysław Jabłoński <sharaquss@gmail.com>
2015-12-06 03:40:49 +0100

    apparently works with 5 nodes too.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main() {
 
     //Model data:
-    int                 elementsCount = 2;
+    int                 elementsCount = 4;
     int                 nodesCount = (elementsCount +1);
     float               globalLength = 5;
     float               globalK = 50;
@@ -29,10 +29,13 @@ int main() {
     //Feeding containers with data:
     elementsLength[0] = 2.5f;
     elementsLength[1] = 2.5f;
+    elementsLength[2] = elementsLength[3] = 2.5f;
     elementsCrossSection[0] = 2;
     elementsCrossSection[1] = 2;
+    elementsCrossSection[2] = elementsCrossSection[3] = 2;
     elementsConductivity[0] = 50;
     elementsConductivity[1] = 50;
+    elementsConductivity[2] = elementsConductivity[3] = 50;
 
     //Feeding GlobalData static class with global data:
     GlobalData(elementsCount, nodesCount, globalLength, globalS, globalK, alpha, tInfinity, q);
@@ -49,14 +52,20 @@ int main() {
     //NODES creation (count-agnostic):
     nodes[0] = Node(0,q/2);
     nodes[1] = Node(1);
-    nodes[2] = Node(2, 0, alpha, tInfinity);
+    nodes[2] = Node(2);
+    nodes[3] = Node(3);
+    nodes[4] = Node(4, 0, alpha, tInfinity);
 
     //ELEMENTS creation (count-agnostic):
     elements[0] = FiniteElement(0, 2, nodes, elementsLength[0],
                                 elementsCrossSection[0], elementsConductivity[0]);
     elements[1] = FiniteElement(1, 2, nodes, elementsLength[1],
                                 elementsCrossSection[1], elementsConductivity[1]);
-    
+    elements[2] = FiniteElement(2, 2, nodes, elementsLength[2],
+                                elementsCrossSection[2], elementsConductivity[2]);
+    elements[3] = FiniteElement(3, 2, nodes, elementsLength[3],
+                                elementsCrossSection[3], elementsConductivity[3]);
+
 
 
     //Calculating local data for every element.
